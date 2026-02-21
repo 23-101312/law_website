a06d161fa7f9fcc4209bccdb5d75e195400077ce
zeina <23-101312@students.eui.edu.eg>
2026-02-21 16:26:42 +0200

    title component and section_2

---
+++ src/components/smalltitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './smalltitle.css';
+
+const Smalltitle = (props) => {
+  return (
+
+    <h2 className='smalltitle'>{props.smalltitletext}</h2>
+    );
+}
+ 
+export default Smalltitle;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/header';
 import Hero from '../components/hero';
+import Smalltitle from '../components/smalltitle';
+import image1 from '../assets/image1.png';
+import './home.css';
 
 
 const Home = (props) => {
@@ -10,6 +13,27 @@ const Home = (props) => {
 
      <main>
       <Hero />
+      <section className='section_2'>
+        <div className='section_2_content'>
+          <Smalltitle smalltitletext="// About The Firm" />
+
+        <div className='sec2_right'>
+          <h2 className='sec2_title'>Since our inception in 2008, Harrington & Associates has redefined legal service by combining <span className='sec2_greentext'>strategic foresight</span> meticulous analysis, and an unwavering commitment to meaningful results.</h2>
+
+          <div className='sec2_middle'>
+            <p>We are not just legal representatives; we are strategic partners. Our firm was founded on the belief that modern legal challenges require more than just textbook knowledge—they demand creativity, adaptability, and a deep understanding of the global business landscape. </p>
+
+            <p>From high-stakes litigation to complex corporate restructuring, we bring clarity to complexity. Our team of 50+ attorneys operates across 15 countries, ensuring that wherever your business goes, our protection follows.</p>
+          </div>
+
+          <img src={image1} alt="" />
+
+        </div>
+
+        </div>
+        
+      </section>
+      
 
      </main>
 
